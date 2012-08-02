6aafb2e158647d3a769ec50a7c54a04715d9980c
Daniel Buchner <danieljb2@gmail.com>
2012-08-02 09:29:13 -0700

    added updates to toggler code

--- toggler/toggler.js
+++ toggler/toggler.js
@@ -1,42 +1,34 @@
 (function(){
 
-	var attr = 'data-toggle-hide';
+	var attr = 'data-toggled';
 	var toggleState = 'data-toggle-state';
 
 	var getTargets = function(){
-		var target = this.getAttribute('data-targetset').split(' ');
-		var targets = [];
-		if (target.length){
-			target.forEach(function(item){
-				var e = document.getElementById(item);
-				if (e)targets.push(e);
+		var targetset = this.getAttribute('data-targetset');
+		if (targetset) {
+			var targets = targetset.split(' ');
+			targets.forEach(function(item, i){
+				var element = document.getElementById(item);
+				element ? targets[i] = element : delete targets[i];
 			});
-		} else {
-			targets.push(this.nextElementSibling);
 		}
-		return targets;
+		return targets || [this.nextElementSibling];
 	}
 
 	var toggle = function(){
-		var targets = getTargets.call(this);
-		targets.forEach(function(item){
-			if (item.attributes[attr]){
-				item.removeAttribute(attr);
-			} else {
-				item.setAttribute(attr, null);
-			}
+		getTargets.call(this).forEach(function(item){
+			item[(item.attributes[attr] ? 'remove' : 'set') + 'Attribute'](attr, null);
 		});
 	}
+	
+	var flipState = function(){
+		this.setAttribute(toggleState, this.attributes[toggleState] && this.getAttribute(toggleState) == 'open' ? 'close' : 'open');
+	};
 
 	xtag.addEvent(document, 'click:delegate(x-toggler)', function(e){
 		toggle.call(this);
-		if (this.attributes[toggleState] && this.getAttribute(toggleState) == 'open'){
-			this.setAttribute(toggleState,'close');
-		} else {
-			this.setAttribute(toggleState,'open');
-		}
+		flipState.call(this);
 	});
-
 
 	xtag.register('x-toggler', {
 		onCreate: function(){
@@ -44,13 +36,8 @@
 		},
 		onInsert: function(){
 			if (this.attributes[toggleState]){
-				var targets = getTargets.call(this);
-				targets.forEach(function(item){
-					if (force == 'open'){
-						item.removeAttribute(attr);
-					} else if (force == 'close') { 
-						item.setAttribute(attr, null);
-					}
+				getTargets.call(this).forEach(function(item){
+					flipState.call(this);
 				});
 			}
 		},
