f88410023f94957620a84759ef0533de6ea3cb92
Arron Schaar <aschaar@gmail.com>
2012-08-03 15:45:01 -0700

    completed first pass of listview element

--- list-view/listview.js
+++ list-view/listview.js
@@ -2,17 +2,53 @@
 
 	xtag.register('x-listview', {
 		onCreate: function(){
-
+			this.yPos = 0;
+			this.mouseDown = false;			
 		},
-		onInsert: function(){
-			
-		},
-		events:{			
-			
+		events:{
+			'mousemove': function(e){				
+				if (this.mouseDown) {					
+					this.scrollTop = (this.yPos - e.clientY);
+				}
+			},
+			'mousedown': function(e) {
+				this.yPos = this.scrollTop + e.clientY;
+				this.mouseDown = true;
+			},
+			'mouseup': function(e){
+				this.mouseDown = false;
+				this.yPos = 0;
+			},
 		},
 		methods:{
-			
+			getSelected: function(){
+				return xtag.query(this, 'x-listitem[selected]');
+			}
 		}
 	});
 
+
+	xtag.register('x-listitem', {
+		onInsert: function(){
+			xtag.fireEvent(this,"nodeinserted");
+		},
+		events:{
+			'mousedown': function(e) {
+				var self = this;
+				setTimeout(function(){
+					var p = self.parentNode;
+					while(p && p.nodeName != 'X-LISTVIEW' && p.nodeName != 'BODY'){
+						p = p.parentNode;
+					}
+					if (p && p.nodeName == 'X-LISTVIEW' && !p.mouseDown){
+						var none = self.attributes['selected'] ? 
+							self.removeAttribute('selected') : 
+							self.setAttribute('selected', null);
+					}
+					
+				}, 150);
+			}
+		}
+	});
+	
 })();
