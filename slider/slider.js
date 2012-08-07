17e58991d01ca755595510eeddba1badb7abf627
Arron Schaar <aschaar@gmail.com>
2012-08-07 14:54:11 -0700

    working on slider

---
+++ slider/slider.js
@@ -0,0 +1,22 @@
+(function(){
+	
+	xtag.register('x-slider', {
+		content: '',
+		onCreate: function(){
+			
+		},
+		onInsert: function(){
+			
+		},
+		setters: {
+			
+		},
+		getters: {
+			
+		},
+		methods: {
+		
+		}
+	});
+
+})();
